3802455632b5d3942cd5dc4a1563938c8f853a56
Myosotis <g20170282@icloud.com>
2021-09-20 09:13:52 -0400

    new week

---
+++ week0920/src/Leetcode0920.java
@@ -0,0 +1,19 @@
+public class Leetcode0920 {
+
+
+
+    // [M] 198      House Robber
+    public int rob(int[] nums) {
+        int N = nums.length;
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
